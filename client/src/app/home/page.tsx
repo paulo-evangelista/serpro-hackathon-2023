475ceaa763f9171f7f2d78c0fd4f4ca5745b12ef
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2023-11-25 18:54:28 -0300

    feat: my titles page

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -81,47 +81,30 @@ export default function Home() {
 	return (
 		<div>
 			<Navbar />
-
+			<div className="relative mt-20 mb-10">
+				<div className="w-full h-80 overflow-hidden">
+				<img
+					src="https://www.tesourodireto.com.br/data/files/7E/31/02/3D/25907810B0345668894D49A8/Banner-1.png"
+					alt="Imagem de fundo"
+					className="w-full h-full object-cover"
+				/>
+				</div>
+				<div className="absolute inset-0 flex items-center justify-start px-32">
+				<div className="flex flex-col">
+					<h1 className="text-white text-4xl font-bold mb-2">
+					Tesouro RendA+
+					</h1>
+					<p className="text-white mb-2">
+					Planeje a sua aposentadoria com o novo título do Tesouro Direto
+					</p>
+					<button className="bg-green-700 text-white px-6 py-3 rounded-lg">
+					Conheça o RendA+ e comece a investir
+					</button>
+				</div>
+				</div>
+			</div>
 			<main className="mt-16 flex flex-col flex-1 justify-center p-2 w-full md:w-3/4 lg:w-3/5 mx-auto">
-        <div id="default-carousel" className="relative w-full" data-carousel="slide">
-          <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="https://www.tesourodireto.com.br/data/files/83/14/98/D5/E684B810D23EF3B8894D49A8/Banner%20TD%20Coletivo.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-			  </div>
-              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="https://www.tesourodireto.com.br/data/files/DF/10/1F/21/83EAA810862B1E98894D49A8/Banner%20sorteio.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-              </div>
-              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="https://www.tesourodireto.com.br/data/files/DE/92/C2/DC/67999810A81F1098894D49A8/banner-educa-mais.webp" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-              </div>
-              <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                  <img src="https://www.tesourodireto.com.br/data/files/7E/31/02/3D/25907810B0345668894D49A8/Banner-1.png" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-              </div>
-          </div>
-          <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-              <button type="button" className="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-              <button type="button" className="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-          </div>
-          <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-              <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                  <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
-                  </svg>
-                  <span className="sr-only">Previous</span>
-              </span>
-          </button>
-          <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-              <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                  <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-                  </svg>
-                  <span className="sr-only">Next</span>
-              </span>
-          </button>
-        </div>
+				
 				<h1 className="text-4xl text-gray-700 font-semibold mb-8">
 					Confira a rentabilidade de cada título
 				</h1>
