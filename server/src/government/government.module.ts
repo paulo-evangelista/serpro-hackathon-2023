dc9ab3f8dafb6275335a216f781ba38aa8352ef5
vict0rcarvalh0 <victordecarvalho342@gmail.com>
2023-11-27 00:08:48 -0300

    feat(server): :sparkles: deploy contract function

--- server/src/government/government.module.ts
+++ server/src/government/government.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Government_User } from 'src/entities/government_user.entity';
 import { Company } from 'src/entities/company.entity';
 import { Asset_Pre_I } from 'src/entities/asset-pre-i.entity';
+import { Web3Module } from 'src/web3/web3.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Government_User, Company, Asset_Pre_I])],
+    imports: [TypeOrmModule.forFeature([Government_User, Company, Asset_Pre_I, Web3Module])],
     controllers: [GovernmentController],
     providers: [GovernmentService],
 })
