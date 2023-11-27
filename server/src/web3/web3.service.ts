db83cb2b698092e6bc61168f181306840bd80d16
Paulo <paulo.evangelista@sou.inteli.edu.br>
2023-11-26 21:27:41 -0300

    refactor(ethereum, server): 🎨 misc

--- server/src/web3/web3.service.ts
+++ server/src/web3/web3.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ethers } from 'ethers';
+import axios from 'axios';
+
 @Injectable()
 export class Web3Service {
     private provider: ethers.InfuraProvider;
@@ -19,4 +21,40 @@ export class Web3Service {
             privateKey: wallet.privateKey,
         };
     }
+
+    sendFileToIPFS = async (file, counter) => {
+        counter++;
+    
+        if (file) {
+            const formData = new FormData();
+            formData.append("file", file);
+    
+            const resFile = await axios({
+                method: "post",
+                url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+                data: formData,
+                headers: {
+                    pinata_api_key: "bf67cf4376213d9d9cb0", // `${process.env.REACT_APP_PINATA_API_KEY}`,
+                    pinata_secret_api_key:
+                        "5250eddb652c2e750bdf57d8ed79ee762564fed74c4ebfd78bb35dd4dbbe5a17", // `${process.env.REACT_APP_PINATA_API_SECRET}`,
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+    
+            const fileHash = `https://ipfs.io/ipfs/${resFile.data.IpfsHash}`;
+    
+            const json = {
+                name: "Profile Image" + counter.toString(),
+                image: fileHash,
+                attributes: [
+                    {
+                        "trait-type": "Stage" + counter.toString(),
+                        value: counter.toString(),
+                    },
+                ],
+            };
+    
+            return json, fileHash;
+        }
+    };
 }
